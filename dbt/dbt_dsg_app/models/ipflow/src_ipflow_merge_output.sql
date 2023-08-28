dfdae4fad65868ffae098a26e631a4cec6c01c09
Adedayo Okubanjo <dayookubanjo@yahoo.com>
2023-08-28 12:33:17 -0400

    Delete unused models

--- dbt/dbt_dsg_app/models/ipflow/src_ipflow_merge_output.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-    materialized = 'table' 
-) }}
-
-select user_ip, min(last_response_code) as last_response_code,
-       max(last_query_date) as last_query_date, any_value(api_response) as api_response 
-       from {{ var('env') }}_IP_FLOW.RAW_DATA.IP_FLOW_API_OUTPUT_DATA 
-      group by user_ip
--- dbt/dbt_dsg_app/models/ipflow/stg_ipflow_success_data.sql
+++
@@ -1,6 +0,0 @@
-{{ config(
-    materialized = 'table'
-) }}
-
-select * from DEV_IP_FLOW.RAW_DATA.IP_FLOW_API_OUTPUT_DATA 
-where last_response_code = 200
